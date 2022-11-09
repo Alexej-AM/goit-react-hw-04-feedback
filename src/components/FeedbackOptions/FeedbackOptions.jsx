e0573f07b95f23ad433c7febb2c38e212433e5ff
Alexej Melnyk <98652436+Alexej-AM@users.noreply.github.com>
2022-11-09 21:06:00 +0200

    work done reload

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import {Feedback, List, Button} from './FeedbackOptionsStyled'
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) =>  {
